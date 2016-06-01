1d664b18b52af18d5623cba0363e34c272c59682
ilya-molostov <ilya.molostov@bk.ru>
2016-06-01 20:11:17 +0700

    ЧитаОблГаз

---
+++ chitaoblgaz/index.php
@@ -0,0 +1,452 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+<!--  шапка 320 --->
+<div id="block_1">
+<div class="container  visible-sm visible-xs no">
+  <div class="row pad-top-10 pad-bot-10">
+    <div id="logo-m" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no">  
+       
+       <!--- -->
+      <div style="float:left; clear:right; position:absolute;">
+      
+<div class="dropdown">
+ <button class="dropdown-toggle" type="button" data-toggle="dropdown">
+ <img src="images/i-m.png" width="31" height="25">
+</button>
+<ul class="dropdown-menu">
+     <li><a href="#">О компании</a></li>
+     <li><a href="#">Новости</a></li>
+     <li><a href="#">Услуги</a></li>
+     <li><a href="#">Кабинет абонента</a></li>
+     <li><a href="#">Промышленная безопасноcть</a></li>
+     <li><a href="#">Карта газовых заправок</a></li>
+     <li class="no-img"><a href="#">Контакты</a></li>
+</ul>
+</div>
+      
+      
+       </div>
+       
+       <a href="#"><div id="login-s"></div></a>
+       
+    </div>
+  </div>
+</div>
+<!--  шапка 320 --->
+<!--  меню --->
+<div id="block_2">
+<div class="container visible-sm visible-xs">
+  <div class="row">
+    <div id="marker" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 pad-bot-10 pad-top-20">
+    
+        <a id="shop_link" href="#"><div class="shop color-w">интернет-магазин</div></a>
+
+    </div>
+  </div>
+</div>
+</div>
+<!--  меню --->
+
+
+
+
+
+
+
+<!--  шапка --->
+<div id="block_1">
+<div class="container hidden-sm hidden-xs">
+  <div class="row pad-top-10 pad-bot-10">
+    <div class="col-lg-5 col-md-4 col-sm-6 col-xs-6">
+      <div id="phone_1" class="phone pad-top-10"><span>3022</span> 26-63-17</div>
+    </div>
+    <div class="col-lg-2 col-md-4 col-sm-6 col-xs-6 text-center">
+      <img src="images/logo_blok_1.png">
+    </div>
+    <div class="col-lg-5 col-md-4 col-sm-6 col-xs-6 pad-top-5">
+      
+      <div style="float:right">
+      <div style="float:left; clear:right;">
+           <a id="shop_link" href="#"><div class="shop">интернет-магазин</div></a>
+      </div>
+      <div id="head_n1">
+          <div id="login">
+            <a id="user" href="#">Войти</a> | <a id="reg" href="#">Регистрация</a>
+          </div>
+      </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+</div>
+<!--  шапка --->
+
+<!--  меню --->
+<div id="block_2">
+<div class="container hidden-sm hidden-xs">
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    
+        <div class="navbar-header">
+           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+            <span>посмотреть меню сайта</span>
+           </button>
+        </div>
+        <div class="collapse navbar-collapse" id="responsive-menu">
+           <ul class="nav navbar-nav">
+             <li><a href="#">О компании</a></li>
+             <li><a href="#">Новости</a></li>
+             <li><a href="#">Услуги</a></li>
+             <li><a href="#">Кабинет абонента</a></li>
+             <li><a href="#">Промышленная безопасноcть</a></li>
+             <li><a href="#">Карта газовых заправок</a></li>
+             <li class="no-img"><a href="#">Контакты</a></li>
+           </ul>
+        </div>      
+
+    </div>
+  </div>
+</div>
+</div>
+<!--  меню --->
+
+<!-- блок с слайдером --->
+<div id="block_3">
+<div class="container">
+  <div class="row slider_block">
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 res"></div>
+    <div id="dd" class="col-lg-6 col-md-6 col-sm-12 col-xs-12 no">
+         <!-- -->
+         <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="2500">
+         <!--индификатор слайдов-->
+         <ol class="carousel-indicators">
+           <li class="active" data-target="#carousel" data-slide-to="0"></li>
+           <li data-target="#carousel" data-slide-to="1"></li>
+           <li data-target="#carousel" data-slide-to="2"></li>
+         </ol>
+        
+         <!-- Слайды -->
+        
+         <div class="carousel-inner no">
+         
+           <div class="item active">
+             <img class="slider-img hidden-xs" src="images/slide_0.jpg" alt="">  
+             <img class="slider-img visible-xs" src="images/img-s.png" alt="">
+             <div class="carousel-caption">
+               <h3>читаоблгаз</h3>
+               <p>обеспечиваем газообразным топливом<br/>Забайкальский край</p>
+             </div>
+           </div>
+           <div class="item">
+             <img class="slider-img hidden-xs" src="images/slide_0.jpg" alt="">  
+             <img class="slider-img visible-xs" src="images/img-s.png" alt="">
+             <div class="carousel-caption">
+               <h3>читаоблгаз</h3>
+               <p>обеспечиваем газообразным топливом<br/>Забайкальский край</p>
+             </div>
+           </div>
+           <div class="item">
+             <img class="slider-img hidden-xs" src="images/slide_0.jpg" alt="">  
+             <img class="slider-img visible-xs" src="images/img-s.png" alt="">
+             <div class="carousel-caption">
+               <h3>читаоблгаз</h3>
+               <p>обеспечиваем газообразным топливом<br/>Забайкальский край</p>
+             </div>
+           </div>
+        
+         </div>
+          
+         <!-- Стрелки преключения слайдов -->
+         <a href="#carousel" class="left carousel-control" data-slide="prev">
+           <span class="st"><img src="images/prev.png"></span>
+         </a>
+        
+         <a id="rg" href="#carousel" class="right carousel-control" data-slide="next">
+           <span  class="st"><img src="images/next.png"></span>
+        </a> 
+        </div>
+        <!-- -->
+    </div>
+    <div id="dd1" class="col-lg-4 col-md-4 col-sm-12 col-xs-12 no">
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no">
+        <div id="banner_1">
+        <a href="#">
+        <span>Узнать</span><br/>
+        задолженность<br/>
+        по газу
+        </a>
+        <a href="#">
+        <div class="btn-links_home">
+         Узнать →
+        </div>
+        </a>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no">
+        <div id="banner_2">
+        <a href="#">
+        <span>газ</span><br/>
+        сжиженный<br/>
+        в баллонах
+        </a>
+        <a href="#">
+        <div class="btn-links_home">
+         Узнать →
+        </div>
+        </a>
+        </div>
+      </div>
+    </div> 
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 res"></div>
+  </div>
+</div>
+</div>
+<!-- блок с слайдером --->
+
+<!--  иконки услуг --->
+<div id="block_4">
+<div class="container">
+  <div style="margin-left:0px; margin-right:0px;" class="row">
+    <div id="t" class="title_block text-center">Наши услуги</div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 res"></div>
+      <div id="dd2" class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+          <a href="#">
+          <div id="i1">
+            Переоборудование<br/>
+            газобаллонных<br/>
+            автомобилей
+          </div>
+          </a>
+      </div>
+      <div id="dd3" class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+          <a href="#">
+          <div id="i2">
+            газификация<br/>
+            и<br/>
+            установка
+          </div>
+          </a>
+      </div>
+      <div id="dd4" class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+         <a href="#">
+         <div id="i3">
+            агзс
+         </div>
+         </a>
+      </div>
+      <div id="dd5" class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+         <a href="#">
+         <div id="i4">
+            сжиженный газ<br/>
+            в баллонах
+         </div>
+         </a>
+      </div>
+      <div id="dd6" class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+          <a href="#">
+          <div id="i5">
+            обслуживание<br/>
+            газового оборудования,<br/>
+            газобалонные установки
+          </div>
+          </a>
+      </div>
+    
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 res"></div>
+    
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    
+        <!--наши услуги-->
+
+    </div>
+  </div>
+</div>
+</div>
+<!--  иконки услуг --->
+
+<!--  страница --->
+<div id="block_5">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 res"></div>
+    <div id="dd7" class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+      <div class="title">о компании</div>
+<div class="text_block">      
+<p>Основным видом хозяйственной деятельности ОАО «Читаоблгаз» является распределение газообразного топлива.</p>
+
+<p>Деятельность Общества направлена на обеспечение услугами газоснабжения потребителей Забайкальского края, дальнейшего развития и надлежащей эксплуатации систем газоснабжения, а также эффективного использования производственного потенциала, рентабельной работы подразделений и Общества в целом.</p>
+
+<p>Основным направлением  деятельности Общества является реализация сжиженного газа, оказание услуг производственного характера. Прочая деятельность Общества – розничная торговля газовым оборудованием.</p>
+</div>
+    </div>
+    <div id="dd8" class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+
+
+<table width="100%" border="0">
+  <tr>
+    <td><div class="title_news">новости</div></td>
+    <td class="tbbb"><a id="nx" data-slide="prev" href="#myCarousel-1"><img src="images/prev.png"></a></td>
+    <td class="tbbb"><a id="nx1" data-slide="next" href="#myCarousel-1"><img src="images/next.png"></a></td>
+  </tr>
+</table>
+
+
+		 
+    <div class="carousel slide" id="myCarousel-1">
+
+        <div class="carousel-inner">
+           <!-------->
+           <div class="item active">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ppn no">
+                 <div class="img_box center-block pk1" style="background-image:url(images/news_1.jpg);">
+                  
+                  <div class="data text-center">
+                    <span>12</span><br/>
+                    апр.
+                  </div>
+                  <div class="info-block">
+                   Информация для<br/>
+				   владельцев ГБО
+                  </div>
+                 </div>
+               </div>
+              <div class="col-lg-4 col-md-4 hidden-sm hidden-xs ppn no">
+                 <div class="img_box center-block pk2" style="background-image:url(images/news_2.jpg);">
+                 <div class="data text-center">
+                    <span>12</span><br/>
+                    апр.
+                  </div>
+                  <div class="info-block">
+                   Заказ товаров нашего<br/>
+                   магазина на наших<br/>
+                   газовых участках
+                  </div>
+                 </div> 
+              </div>
+              <div class="col-lg-4 col-md-4 hidden-sm hidden-xs ppn no">
+                 <div class="img_box center-block pk3" style="background-image:url(images/news_3.jpg);">
+                   <div class="data text-center">
+                    <span>12</span><br/>
+                    апр.
+                  </div>
+                  <div class="info-block">
+                   Новые способы<br/>
+				   оплаты
+                  </div>
+                 </div>
+               </div>
+           </div> 
+            <!-------->
+            <!-------->
+           <div class="item">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ppn no">
+                 <div class="img_box center-block pk1" style="background-image:url(images/news_1.jpg);">
+                  
+                  <div class="data text-center">
+                    <span>12</span><br/>
+                    апр.
+                  </div>
+                  <div class="info-block">
+                   Информация для<br/>
+				   владельцев ГБО
+                  </div>
+                 </div>
+               </div>
+              <div class="col-lg-4 col-md-4  hidden-sm hidden-xs ppn no">
+                 <div class="img_box center-block pk2" style="background-image:url(images/news_2.jpg);">
+                 <div class="data text-center">
+                    <span>12</span><br/>
+                    апр.
+                  </div>
+                  <div class="info-block">
+                   Заказ товаров нашего<br/>
+                   магазина на наших<br/>
+                   газовых участках
+                  </div>
+                 </div> 
+              </div>
+              <div class="col-lg-4 col-md-4 hidden-sm hidden-xs ppn no">
+                 <div class="img_box center-block pk3" style="background-image:url(images/news_3.jpg);">
+                   <div class="data text-center">
+                    <span>12</span><br/>
+                    апр.
+                  </div>
+                  <div class="info-block">
+                   Новые способы<br/>
+				   оплаты
+                  </div>
+                 </div>
+               </div>
+           </div> 
+            <!-------->
+            
+        </div>                         
+    </div>
+      
+      
+    </div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 res"></div>
+  </div>
+</div>
+</div>
+<!--  страница --->
+
+<!--  подвал --->
+<div id="block_6">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+      <div class="copir">
+          <div class="pad-bot-10">© 2016. Все права защищены</div>
+          Разработка <a href="#">«Форест»</a>
+      </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+      <ul class="inline">
+       <li><a href="#">О компании</a></li>
+       <li><a href="#">Новости</a></li>
+       <li><a href="#">Услуги</a></li>
+       <li><a href="#">Кабинет абонента</a></li>
+       <li><a href="#">Промышленная безопасноcть</a></li>
+       <li><a href="#">Карта газовых заправок</a></li>
+       <li><a href="#">Контакты</a></li>
+      </ul>                                              
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+    <div id="f-3">
+      <div id="m-10" class="phone pad-bot-10">3022 26-63-17</div>
+      <a id="shop_link_1" href="#"><div id="v" class="shop">интернет-магазин</div></a>
+    </div>
+    </div>
+  </div>
+</div>
+</div>
+<!--  подвал --->
+  
+  
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ chitaoblgaz/lc.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+<!--  шапка 320 --->
+<div id="block_1">
+<div class="container  visible-sm visible-xs no">
+  <div class="row pad-top-10 pad-bot-10">
+    <div id="logo-m" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no">  
+       
+       <!--- -->
+      <div style="float:left; clear:right; position:absolute;">
+<div class="dropdown">
+ <button class="dropdown-toggle" type="button" data-toggle="dropdown">
+ <img src="images/i-m.png" width="31" height="25">
+</button>
+<ul class="dropdown-menu">
+     <li><a href="#">О компании</a></li>
+     <li><a href="#">Новости</a></li>
+     <li><a href="#">Услуги</a></li>
+     <li><a href="#">Кабинет абонента</a></li>
+     <li><a href="#">Промышленная безопасноcть</a></li>
+     <li><a href="#">Карта газовых заправок</a></li>
+     <li class="no-img"><a href="#">Контакты</a></li>
+</ul>
+</div>    
+       <!--- -->
+       </div>
+       
+       <a href="#"><div id="login-s"></div></a>
+       
+    </div>
+  </div>
+</div>
+<!--  шапка 320 --->
+<!--  меню --->
+<div id="block_2">
+<div class="container visible-sm visible-xs">
+  <div class="row">
+    <div id="marker" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 pad-bot-10 pad-top-20">
+    
+        <a id="shop_link" href="#"><div class="shop color-w">интернет-магазин</div></a>
+
+    </div>
+  </div>
+</div>
+</div>
+<!--  меню --->
+
+
+
+
+
+
+
+<!--  шапка --->
+<div id="block_1">
+<div class="container hidden-sm hidden-xs">
+  <div class="row pad-top-10 pad-bot-10">
+    <div class="col-lg-5 col-md-4 col-sm-6 col-xs-6">
+      <div id="phone_1" class="phone pad-top-10"><span>3022</span> 26-63-17</div>
+    </div>
+    <div class="col-lg-2 col-md-4 col-sm-6 col-xs-6 text-center">
+      <img src="images/logo_blok_1.png">
+    </div>
+    <div class="col-lg-5 col-md-4 col-sm-6 col-xs-6 pad-top-5">
+      
+      <div style="float:right">
+      <div style="float:left; clear:right;">
+           <a id="shop_link" href="#"><div class="shop">интернет-магазин</div></a>
+      </div>
+      <div id="head_n1">
+          <div id="login">
+            <a id="user" href="#">Константин Алексеев</a> 
+            <a href="#"><img src="images/1_lc.jpg" width="25" height="25"></a>
+            <a href="#"><img src="images/2_lc.jpg" width="25" height="25"></a>
+            </div>
+      </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+</div>
+<!--  шапка --->
+
+<!--  меню --->
+<div id="block_2">
+<div class="container hidden-sm hidden-xs">
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    
+        <div class="navbar-header">
+           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+            <span>посмотреть меню сайта</span>
+           </button>
+        </div>
+        <div class="collapse navbar-collapse" id="responsive-menu">
+           <ul class="nav navbar-nav">
+             <li><a href="#">О компании</a></li>
+             <li><a href="#">Новости</a></li>
+             <li><a href="#">Услуги</a></li>
+             <li><a href="#">Кабинет абонента</a></li>
+             <li><a href="#">Промышленная безопасноcть</a></li>
+             <li><a href="#">Карта газовых заправок</a></li>
+             <li class="no-img"><a href="#">Контакты</a></li>
+           </ul>
+        </div>      
+
+    </div>
+  </div>
+</div>
+</div>
+<!--  меню --->
+
+<!--  страница --->
+<div class="container">
+  <div class="row">
+     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 pad-30x text-center">
+       <div class="t-big">личный кабинет</div>
+       <div class="t-sll">ФИЗ. ЛИЦО</div>
+     </div>
+   </div>
+</div>
+
+
+<!--  страница --->
+<div class="container">
+  <div class="row">
+     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 pad-30x">
+       <div class="bg_banner_1">
+         <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
+           <div class="block-222">	
+            <span>Узнать</span>
+			задолженность<br/>
+			по газу
+           </div> 
+         </div>
+         <div class="col-lg-6 col-md-7 col-sm-12 col-xs-12">
+           <form id="dogovor">
+             <input type="text" class="input_lc" placeholder="номер договора">
+             <a href="#"><span id="xlc">Узнать</span></a>
+           </form>
+           
+           <p class="text-center title-x">Ваша задолжность: 578 Руб.</p>
+         </div>
+         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+           
+         </div>
+       </div>
+   </div>
+     
+     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 pad-30x hidden-sm hidden-xs">
+        <a class="color-wh" href="#"><div class="bg_banner_2">
+          <div class="block-333">
+           <span>газ</span><br/>
+           сжиженный<br/>
+           в баллонах
+          </div>
+        </div></a>
+    </div>
+     <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 pad-30x">
+        <div class="news_lc" style="background-image:url(images/news_333.jpg);">
+        </div>
+        <div class="data_lc text-center">
+          <span>12</span>
+          апр.
+        </div>
+        <div class="block_news_lc">
+          <div class="news_title">НОВОСТИ</div>
+          
+          Заказ товаров нашего<br/>
+		  магазина на наших<br/>
+          газовых участках
+        </div>
+     </div>
+  </div>
+</div>
+<!--  страница --->
+
+<div class="h100_cv"></div>
+
+<!--  подвал --->
+<div id="block_6">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+      <div class="copir">
+          <div class="pad-bot-10">© 2016. Все права защищены</div>
+          Разработка <a href="#">«Форест»</a>
+      </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+      <ul class="inline">
+       <li><a href="#">О компании</a></li>
+       <li><a href="#">Новости</a></li>
+       <li><a href="#">Услуги</a></li>
+       <li><a href="#">Кабинет абонента</a></li>
+       <li><a href="#">Промышленная безопасноcть</a></li>
+       <li><a href="#">Карта газовых заправок</a></li>
+       <li><a href="#">Контакты</a></li>
+      </ul>                                              
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+    <div id="f-3">
+      <div id="m-10" class="phone pad-bot-10">3022 26-63-17</div>
+      <a id="shop_link_1" href="#"><div id="v" class="shop">интернет-магазин</div></a>
+    </div>
+    </div>
+  </div>
+</div>
+</div>
+<!--  подвал --->
+  
+  
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
